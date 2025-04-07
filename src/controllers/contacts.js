b512eccab67714e0ca4d10ef3fdc6ff6c7fb4365
Artem <suleymanov2007artem@gmail.com>
2025-04-07 14:45:04 +0300

    resolve req problem

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -13,13 +13,11 @@ export const getContactsController = async (req, res) => {
   export const getContactsByIdController = async (req, res) => {
     const { contactId } = req.params;
     const contact = await getContactById(contactId);
-
+  
     if (!contact) {
-      return res.status(404).json({
-        message: 'Contact not found',
-      });
+      throw createHttpError(404, "Contact not found");
     }
-
+  
     res.status(200).json({
       status: 200,
       message: `Successfully found contact with id ${contactId}!`,
