3d969fce54fe25b0e44c6e7bb6c9669f84492635
Artem <suleymanov2007artem@gmail.com>
2025-04-10 17:15:45 +0300

    add joi validate

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,5 +1,6 @@
 import { getContacts, getContactById, addContact, updateContactById, deleteContactById } from '../services/contacts.js';
 import createHttpError from 'http-errors';
+import { contactAddSchema } from '../validation/contacts.js';
 
 export const getContactsController = async (req, res) => {
     const contacts = await getContacts();
@@ -26,6 +27,13 @@ export const getContactsController = async (req, res) => {
   };
 
   export const addContactController = async(req,res) =>{
+    try {
+      await contactAddSchema.validate(req.body, {
+        abortEarly: false,
+      });
+    } catch (error) {
+      throw createHttpError(400, error.message);
+    }
     const data = await addContact(req.body);
     res.status(201).json({
       status: 201,
