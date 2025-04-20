f2472ef65c1b8dd66cec53f7aba04008e1411e18
Artem <suleymanov2007artem@gmail.com>
2025-04-20 22:50:19 +0300

    add password hashing with bcrypt

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,5 +1,22 @@
+import createHttpError from "http-errors";
 import { UsersCollection } from "../db/models/user.js";
+import bcrypt from "bcrypt";
+const SALT_ROUNDS = 10;
 
-export const registerUser = async (payload) => {
-    return await UsersCollection.create(payload);
+export const registerUser = async ({ name, email, password }) => {
+    const existingUser = await UsersCollection.findOne({email});
+
+    if (existingUser) {
+      throw createHttpError(409, "Email in use");
+    }
+
+    const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
+
+    const newUser = await UsersCollection.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+  
+    return newUser;
   };
